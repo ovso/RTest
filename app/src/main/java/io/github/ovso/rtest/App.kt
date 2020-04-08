df7ac2764fc58c1373d26fa8b9356df3008a37bc
peterjaeho <peter@mybroccoli.co.kr>
2020-04-08 22:31:05 +0900

    초기 설정

---
+++ app/src/main/java/io/github/ovso/rtest/App.kt
@@ -0,0 +1,10 @@
+package io.github.ovso.rtest
+
+import android.app.Application
+
+class App : Application() {
+
+  override fun onCreate() {
+    super.onCreate()
+  }
+}
---
+++ app/src/main/java/io/github/ovso/rtest/view/ui/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package io.github.ovso.rtest.view.ui.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.github.ovso.rtest.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

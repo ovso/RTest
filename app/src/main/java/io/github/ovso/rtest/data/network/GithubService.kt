a141f455f77981119b08bfcc60ed8be474a414e9
jaeho <ovsoce@gmail.com>
2020-04-10 09:03:19 +0900

    유닛테스트 : 깃허브 사용자 저장소 목록과 목록별 스타게이저 정보 가져오기

--- app/src/main/java/io/github/ovso/rtest/data/network/GithubService.kt
+++ app/src/main/java/io/github/ovso/rtest/data/network/GithubService.kt
@@ -1,6 +1,7 @@
 package io.github.ovso.rtest.data.network
 
 import io.github.ovso.rtest.data.network.model.Repo
+import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -14,6 +15,13 @@ interface GithubService {
     @Query("per_page") per_page: Int = 5
   ): Single<List<Repo>>
 
+  @GET("/users/{user}/repos")
+  fun userRepos2(
+    @Path("user") user: String,
+    @Query("page") page: Int = 1,
+    @Query("per_page") per_page: Int = 5
+  ): Observable<List<Repo>>
+
   @GET("/repos/{user}/{repo}/stargazers")
   fun stargazers(
     @Path("user") user: String,
@@ -21,4 +29,12 @@ interface GithubService {
     @Query("page") page: Int = 1,
     @Query("per_page") per_page: Int = 5
   ): Single<Any>
+
+  @GET("/repos/{user}/{repo}/stargazers")
+  fun stargazers2(
+    @Path("user") user: String,
+    @Path("repo") repo: String,
+    @Query("page") page: Int = 1,
+    @Query("per_page") per_page: Int = 5
+  ): Observable<Any>
 }
--- app/src/test/java/io/github/ovso/rtest/Github.kt
+++ app/src/test/java/io/github/ovso/rtest/Github.kt
@@ -5,7 +5,9 @@ package io.github.ovso.rtest
 import io.github.ovso.rtest.data.network.GithubRepository
 import io.github.ovso.rtest.data.network.User
 import io.github.ovso.rtest.data.network.model.Repo
+import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Scheduler
+import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.Schedulers
 import org.junit.Test
 
@@ -23,14 +25,14 @@ class Github {
       println(t.message)
     }
 
-    repo.api().userRepos(User.name, 1, 5)
+    repo.api().userRepos(User.name, 1, 3)
       .subscribeOn(SchedulerProvider.io())
       .observeOn(SchedulerProvider.ui())
       .subscribe(::onSuccess, ::onFailure)
   }
 
   @Test
-  fun `깁허브 저장소에 스타를 준 stargazer 목록`() {
+  fun `깃허브 저장소에 스타를 준 stargazer 목록`() {
 
     fun onSuccess(items: Any) {
       println(items)
@@ -41,6 +43,30 @@ class Github {
     }
 
     repo.api().stargazers(User.name,"android-PermissionRequest")
+      .subscribeOn(SchedulerProvider.io())
+      .observeOn(SchedulerProvider.ui())
+      .subscribe(::onSuccess, ::onFailure)
+  }
+
+  @Test
+  fun `깃허브 사용자 저장소 목록과 목록별 스타게이저 정보 가져오기`() {
+
+    fun onSuccess(items: Any) {
+      println(items.toString())
+    }
+
+    fun onFailure(t: Throwable) {
+      println(t.message)
+    }
+
+    repo.api().userRepos2(User.name, 1, 1)
+      .flatMap {
+        Observable.fromIterable(it)
+      }.flatMap {
+        repo.api().stargazers2(User.name, it.name)
+      }.map {
+        it
+      }
       .subscribeOn(SchedulerProvider.io())
       .observeOn(SchedulerProvider.ui())
       .subscribe(::onSuccess, ::onFailure)

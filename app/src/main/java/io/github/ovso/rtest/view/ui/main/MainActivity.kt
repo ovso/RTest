c1976766761c7a1f6879dfb0825025aa1ea5aebd
jaeho <ovsoce@gmail.com>
2020-04-11 12:49:06 +0900

    Hide supportActionBar on Main

--- app/src/main/java/io/github/ovso/rtest/view/ui/main/MainActivity.kt
+++ app/src/main/java/io/github/ovso/rtest/view/ui/main/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+    supportActionBar?.hide()
     vp_main.adapter = MainPagerAdapter(
       resources.getStringArray(R.array.tab_names),
       supportFragmentManager

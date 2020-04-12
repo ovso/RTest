d67c3e45c3ece5c5a9d4323edf6b58fda273dd53
jaeho <ovsoce@gmail.com>
2020-04-12 20:08:32 +0900

    A영역 예외처리

--- app/src/main/java/io/github/ovso/rtest/view/ui/area_a/AAreaViewModel.kt
+++ app/src/main/java/io/github/ovso/rtest/view/ui/area_a/AAreaViewModel.kt
@@ -5,46 +5,53 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import io.github.ovso.rtest.R
 import io.github.ovso.rtest.data.network.GithubRepository
-import io.github.ovso.rtest.data.network.User
 import io.github.ovso.rtest.data.network.model.Repo
-import io.github.ovso.rtest.data.network.model.ShareModel
 import io.github.ovso.rtest.exts.plusAssign
 import io.github.ovso.rtest.utils.rx.RxBus
 import io.github.ovso.rtest.utils.rx.SchedulerProvider
 import io.github.ovso.rtest.view.base.DisposableViewModel
-import timber.log.Timber
 
 class AAreaViewModel : DisposableViewModel() {
 
   private val navigateToNewScreen = MutableLiveData<String>()
   private val message = MutableLiveData<Int>()
   private val repository by lazy { GithubRepository() }
+  private var userName: String? = null
 
   init {
     observe()
   }
 
-  val avatarUrl = ObservableField<String>()
-  val name = ObservableField<String>()
+  val displayAvatarUrl = ObservableField<String>()
+  val displayName = ObservableField<String>()
 
   private fun observe() {
     compositeDisposable += RxBus.toObservable().subscribe {
       if (it is AAreaModel) {
-        avatarUrl.set(it.avatarUrl)
-        name.set(it.name)
+        displayAvatarUrl.set(it.avatarUrl)
+        displayName.set(it.name)
+        userName = it.name
       }
     }
   }
 
   fun onClick() {
+    userName?.let {
+      reqUserRepos(it)
+    } ?: run {
+      message.value = R.string.empty_data
+    }
+  }
+
+  private fun reqUserRepos(userName:String) {
     fun onSuccess(repos: List<Repo>) {
       if (repos.isNullOrEmpty()) {
         message.value = R.string.empty_data
       } else {
-        navigateToNewScreen.value = name.get()
+        navigateToNewScreen.value = displayName.get()
       }
     }
-    repository.api().userRepos(User.name, 1, 30)
+    repository.api().userRepos(userName, 1, 30)
       .subscribeOn(SchedulerProvider.io())
       .observeOn(SchedulerProvider.ui())
       .subscribe(::onSuccess) { println(it); message.value = R.string.empty_data }

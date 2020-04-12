cf4ee6d109d47004abb3cf75d8f0d0e1f33c3956
jaeho <ovsoce@gmail.com>
2020-04-12 18:22:07 +0900

    상단 영역 로직 구현중

--- app/src/main/java/io/github/ovso/rtest/view/ui/area_a/AAreaFragment.kt
+++ app/src/main/java/io/github/ovso/rtest/view/ui/area_a/AAreaFragment.kt
@@ -2,9 +2,11 @@ package io.github.ovso.rtest.view.ui.area_a
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -48,6 +50,11 @@ class AAreaFragment : Fragment() {
       User.name = it
       startActivity(Intent(context, MainActivity::class.java))
       requireActivity().finish()
+    })
+    viewModel.getMessage().observe(viewLifecycleOwner, Observer {
+      val makeText = Toast.makeText(context, it, Toast.LENGTH_SHORT)
+      makeText.setGravity(Gravity.TOP, 0, 0)
+      makeText.show()
     })
   }
 }
--- app/src/main/java/io/github/ovso/rtest/view/ui/area_a/AAreaViewModel.kt
+++ app/src/main/java/io/github/ovso/rtest/view/ui/area_a/AAreaViewModel.kt
@@ -3,14 +3,22 @@ package io.github.ovso.rtest.view.ui.area_a
 import androidx.databinding.ObservableField
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import io.github.ovso.rtest.R
+import io.github.ovso.rtest.data.network.GithubRepository
+import io.github.ovso.rtest.data.network.User
+import io.github.ovso.rtest.data.network.model.Repo
+import io.github.ovso.rtest.data.network.model.ShareModel
 import io.github.ovso.rtest.exts.plusAssign
 import io.github.ovso.rtest.utils.rx.RxBus
+import io.github.ovso.rtest.utils.rx.SchedulerProvider
 import io.github.ovso.rtest.view.base.DisposableViewModel
 import timber.log.Timber
 
 class AAreaViewModel : DisposableViewModel() {
 
   private val navigateToNewScreen = MutableLiveData<String>()
+  private val message = MutableLiveData<Int>()
+  private val repository by lazy { GithubRepository() }
 
   init {
     observe()
@@ -29,11 +37,21 @@ class AAreaViewModel : DisposableViewModel() {
   }
 
   fun onClick() {
-    Timber.d("onClick()")
-    navigateToNewScreen.value = name.get()
+    fun onSuccess(repos: List<Repo>) {
+      if (repos.isNullOrEmpty()) {
+        message.value = R.string.empty_data
+      } else {
+        navigateToNewScreen.value = name.get()
+      }
+    }
+    repository.api().userRepos(User.name, 1, 30)
+      .subscribeOn(SchedulerProvider.io())
+      .observeOn(SchedulerProvider.ui())
+      .subscribe(::onSuccess) { println(it); message.value = R.string.empty_data }
   }
 
   fun getNavigateToNewScreen(): LiveData<String> = navigateToNewScreen
+  fun getMessage(): LiveData<Int> = message
 
   data class AAreaModel(
     val avatarUrl: String,
